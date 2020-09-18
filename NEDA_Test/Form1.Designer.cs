34d4505475f54d7fa815327db29e8767ea8f5452
nornion <71500053+nornion@users.noreply.github.com>
2020-09-18 19:55:19 +0800

    Initial release

---
+++ NEDA_Test/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace NEDA_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgv_TestLimit = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dgv_result = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dgv_HBin = new System.Windows.Forms.DataGridView();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.dgv_SBin = new System.Windows.Forms.DataGridView();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.dgv_TestCount = new System.Windows.Forms.DataGridView();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.dgv_PartCount = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_TestLimit)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_result)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_HBin)).BeginInit();
+            this.tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_SBin)).BeginInit();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_TestCount)).BeginInit();
+            this.tabPage7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PartCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 393);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(225, 17);
+            this.labelStatus.Text = "Click [Select File] to choose STDF File.";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 393);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonOpenFile);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.listBox2);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 367);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Lot Info";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(357, 29);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 52);
+            this.buttonOpenFile.TabIndex = 4;
+            this.buttonOpenFile.Text = "Select File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(357, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Extract Async";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(357, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Extract";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(438, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(351, 361);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(351, 361);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dgv_TestLimit);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 367);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Test Limits";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgv_TestLimit
+            // 
+            this.dgv_TestLimit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_TestLimit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_TestLimit.Location = new System.Drawing.Point(3, 3);
+            this.dgv_TestLimit.Name = "dgv_TestLimit";
+            this.dgv_TestLimit.Size = new System.Drawing.Size(786, 361);
+            this.dgv_TestLimit.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dgv_result);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(792, 367);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Test Result";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dgv_result
+            // 
+            this.dgv_result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_result.Location = new System.Drawing.Point(0, 0);
+            this.dgv_result.Name = "dgv_result";
+            this.dgv_result.Size = new System.Drawing.Size(792, 367);
+            this.dgv_result.TabIndex = 1;
+            this.dgv_result.ColumnAdded += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dgv_result_ColumnAdded);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dgv_HBin);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(792, 367);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "HBin info";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // dgv_HBin
+            // 
+            this.dgv_HBin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_HBin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_HBin.Location = new System.Drawing.Point(0, 0);
+            this.dgv_HBin.Name = "dgv_HBin";
+            this.dgv_HBin.Size = new System.Drawing.Size(792, 367);
+            this.dgv_HBin.TabIndex = 1;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.dgv_SBin);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(792, 367);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "SBin Info";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // dgv_SBin
+            // 
+            this.dgv_SBin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_SBin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_SBin.Location = new System.Drawing.Point(0, 0);
+            this.dgv_SBin.Name = "dgv_SBin";
+            this.dgv_SBin.Size = new System.Drawing.Size(792, 367);
+            this.dgv_SBin.TabIndex = 1;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.dgv_TestCount);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(792, 367);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Test Count";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // dgv_TestCount
+            // 
+            this.dgv_TestCount.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_TestCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_TestCount.Location = new System.Drawing.Point(0, 0);
+            this.dgv_TestCount.Name = "dgv_TestCount";
+            this.dgv_TestCount.Size = new System.Drawing.Size(792, 367);
+            this.dgv_TestCount.TabIndex = 1;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.dgv_PartCount);
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(792, 367);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Part Count";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // dgv_PartCount
+            // 
+            this.dgv_PartCount.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_PartCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_PartCount.Location = new System.Drawing.Point(0, 0);
+            this.dgv_PartCount.Name = "dgv_PartCount";
+            this.dgv_PartCount.Size = new System.Drawing.Size(792, 367);
+            this.dgv_PartCount.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Test_data_log.stdf";
+            this.openFileDialog1.Filter = "STDF|*.std*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 415);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "NEDA STDF Parser Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_TestLimit)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_result)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_HBin)).EndInit();
+            this.tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_SBin)).EndInit();
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_TestCount)).EndInit();
+            this.tabPage7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_PartCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.DataGridView dgv_TestLimit;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dgv_result;
+        private System.Windows.Forms.DataGridView dgv_HBin;
+        private System.Windows.Forms.DataGridView dgv_SBin;
+        private System.Windows.Forms.DataGridView dgv_TestCount;
+        private System.Windows.Forms.DataGridView dgv_PartCount;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ NEDA_Test/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.ComponentModel;
+using Nornion;
+
+namespace NEDA_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string stdf_file = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgv_TestLimit.AutoGenerateColumns = true;
+            dgv_result.AutoGenerateColumns = true;
+            dgv_SBin.AutoGenerateColumns = true;
+            dgv_HBin.AutoGenerateColumns = true;
+            dgv_TestCount.AutoGenerateColumns = true;
+            dgv_PartCount.AutoGenerateColumns = true;
+        }
+
+        //Update STDF Extracted data to Form
+        private void RenderStdfResult(NEDA nda)
+        {
+            //Extraction complete and no error detected
+            labelStatus.Text = "Lot: " + nda.StdLotInfo["LOT_ID"].ToString() + " extract completed!";
+            //lot info
+            listBox1.Items.Clear();
+            if (nda.StdLotInfo != null && nda.StdLotInfo.Count > 0)
+            {
+                foreach (string obj in nda.StdLotInfo.Keys)
+                {
+                    string fieldName = obj;
+                    if (fieldName.Length < 30) fieldName = fieldName.PadRight(30 - fieldName.Length);
+                    string filedValue = nda.StdLotInfo[obj].ToString();
+                    if (filedValue.Length < 30) filedValue = filedValue.PadRight(30 - filedValue.Length);
+                    listBox1.Items.Add(fieldName + " --- " + filedValue + " --- " + nda.StdLotInfo[obj].GetType().ToString());
+                }
+            }
+            //Wafer info (if it is a wafer sort STDF)
+            listBox2.Items.Clear();
+            if (nda.StdfWaferInfo != null && nda.StdfWaferInfo.Count > 0)
+            {
+                foreach (string obj in nda.StdfWaferInfo.Keys)
+                {
+                    string fieldName = obj;
+                    if (fieldName.Length < 30) fieldName = fieldName.PadRight(30 - fieldName.Length);
+                    string filedValue = nda.StdfWaferInfo[obj].ToString();
+                    if (filedValue.Length < 30) filedValue = filedValue.PadRight(30 - filedValue.Length);
+                    listBox2.Items.Add(fieldName + " --- " + filedValue + " --- " + nda.StdfWaferInfo[obj].GetType().ToString());
+                }
+            }
+
+            /***************************************************************************************************
+             * All results in Datatable structure, refer to below TableName definition
+             * TestLimits -- Datatable contains definition of all parametric tests [limits and unit]
+             * TestData   -- Datatable contains results of all parametric tests [test reading]
+             * HBin       -- Datatable contains Hardware bin information
+             * SBin       -- Datatable contains software bin information 
+             * TestCount  -- Datatable contains statistical count information [executed, failed...] of all tests
+             * PartCount  -- Datatable contains site executed part count information [normally it is not used]
+             ***************************************************************************************************/
+            foreach (DataTable dt in nda.StdDataSet.Tables)
+            {
+                switch (dt.TableName)
+                {
+                    case "TestLimits": dgv_TestLimit.DataSource = nda.StdDataSet.Tables["TestLimits"]; break;
+                    case "TestData":
+                        {
+                            dgv_result.AutoGenerateColumns = true;
+                            dgv_result.DataSource = nda.StdDataSet.Tables["TestData"];
+                        }
+                        break;
+                    case "SBin": dgv_SBin.DataSource = nda.StdDataSet.Tables["SBin"]; break;
+                    case "HBin": dgv_HBin.DataSource = nda.StdDataSet.Tables["HBin"]; break;
+                    case "TestCount": dgv_TestCount.DataSource = nda.StdDataSet.Tables["TestCount"]; break;
+                    case "PartCount": dgv_PartCount.DataSource = nda.StdDataSet.Tables["PartCount"]; break;
+                    default:; break;
+                }
+            }
+        }
+
+        //Initialize a NEDA instance
+        NEDA nda = new NEDA();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(stdf_file))
+            {
+                //Check Error flag after initialization
+                if (nda.ErrorFlag)
+                {//Some error happened during NEDA initialization, show Error msg
+                    labelStatus.Text = nda.ErrorMsg;
+                }
+                else
+                {
+                    labelStatus.Text = "Start extracting [" + Path.GetFileName(stdf_file) + "], please wait!";
+                    Application.DoEvents();
+
+                    //Start STDF extraction
+                    if (nda.ParseStdf(stdf_file))//check return flag, if return=false, there are errors detected during extracting
+                    {
+                        RenderStdfResult(nda);
+                    }
+                    else
+                    {//Some error happened during extraction, show Error msg
+                        labelStatus.Text = nda.ErrorMsg;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please click [Select File] to select STDF file first!");
+            }
+        }
+
+        /************************** Run Async ***************************/
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(stdf_file))
+            {
+                //Check Error flag after initialization
+                if (nda.ErrorFlag)
+                {//Some error happened during NEDA initialization, show Error msg
+                    labelStatus.Text = nda.ErrorMsg;
+                }
+                else
+                {
+                    labelStatus.Text = "Start extracting [" + Path.GetFileName(stdf_file) + "], please wait!";
+                    Application.DoEvents();
+                    nda.ProgressChangedEvent = new ProgressChangedEventHandler(UpdateExtractProgress);
+                    nda.WorkerCompleteEvent = new RunWorkerCompletedEventHandler(ExtractCompleteCallback);
+                    //Start STDF extraction
+                    nda.ParseStdfAsync(stdf_file);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please click [Select File] to select STDF file first!");
+            }
+        }
+
+        public void UpdateExtractProgress(object sender, ProgressChangedEventArgs e)
+        {
+            labelStatus.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        public void ExtractCompleteCallback(object sender, RunWorkerCompletedEventArgs e)
+        {
+            labelStatus.Text = "Extraction complete!";
+            //check return flag, if return=false, there are errors detected during extracting
+            if (!nda.ErrorFlag)
+            {
+                RenderStdfResult(nda);
+            }
+            else
+            {//Some error happened during extraction, show Error msg
+                labelStatus.Text = nda.ErrorMsg;
+            }
+        }
+
+        /***** Events of Form Controls *******/
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Dispose NEDA object
+            if(nda!=null)
+                nda.Dispose();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.FileName;
+            if(File.Exists(fileName))
+            {
+                stdf_file = fileName;
+                labelStatus.Text = stdf_file;
+            }
+        }
+
+        private void dgv_result_ColumnAdded(object sender, DataGridViewColumnEventArgs e)
+        {
+            e.Column.FillWeight = 1;
+        }
+    }
+}
